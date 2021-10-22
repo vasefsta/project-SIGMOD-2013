49d57f8080af377d0c0b2b6230c4badf0e6b845d
vasiliki2000 <sdi1800229@di.uoa.gr>
2021-10-22 11:40:58 +0300

    add

--- modules/ADTMap.c
+++ modules/ADTMap.c
@@ -1,3 +1,14 @@
 #include <stdlib.h>
 
 #include "ADTMap.h"
+
+MapNode map_create(int size) {
+    MapNode map = malloc(sizeof(*map)*size);
+
+    for (int i = 0; i < size; i++) {
+        map[i].query = NULL;
+        map[i].next = NULL;
+    }
+
+    return map;
+}
