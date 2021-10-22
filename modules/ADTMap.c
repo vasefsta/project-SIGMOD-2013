55ad1020e99ad1997696892e9c420a052ccdba87
Nikos Eftychiou <sdi1800221@di.uoa.gr>
2021-10-22 14:58:10 +0300

    hashtable

--- modules/ADTMap.c
+++ modules/ADTMap.c
@@ -12,3 +12,32 @@ MapNode map_create(int size) {
 
     return map;
 }
+
+int map_insert(MapNode map, Query query) {
+    int position = hash_string(query);
+
+    MapNode temp = &map[position];
+
+    while (map[position].query != NULL){
+        temp = map[position].next;
+
+    }
+
+}
+
+
+int map_destroy(DestroyFunc destroy) {
+
+}
+
+
+int hash_string(String value) {     //Voithitiki sto readme apo chatzi
+
+	// djb2 hash function, απλή, γρήγορη, και σε γενικές γραμμές αποδοτική
+    int hash = 5381;
+    
+    for (char s = value; s != '\0'; s++)
+		hash = (hash << 5) + hash + s;			// hash = (hash  33) + s. Το foo << 5 είναι γρηγορότερη εκδοχή του foo  32.
+    
+    return hash;
+}
