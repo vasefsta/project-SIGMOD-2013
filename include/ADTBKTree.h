b41d96dc0a6813a576dd178674528101eb11b806
Nikos Eftychiou <sdi1800221@di.uoa.gr>
2021-10-26 19:54:59 +0300

    Hamming distance and bkcreate

--- include/ADTBKTree.h
+++ include/ADTBKTree.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "core.h"
 #include "common_types.h"
 
 typedef struct bktree* BKTree;
 
 typedef struct bknode* BKNode;
+
+BKTree create_bk_tree(MatchType type);
+
+ErrorCode insert_bknode(BKTree bktree, Pointer value);
+
+void bk_tree_destroy(BKTree bktree, DestroyFunc destroy);
+
+int hamming_distance(Pointer value1, Pointer value2);
+
+int edit_distance(Pointer value1, Pointer value2);
--- modules/ADTBKTree.c
+++ modules/ADTBKTree.c
@@ -1,5 +1,8 @@
 #include "ADTBKTree.h"
 #include "ADTLinkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct bknode {
     Pointer value;
@@ -9,6 +12,55 @@ struct bknode {
 struct bktree {
     BKNode root;
     CompareFunc compare;
-    DestroyFunc destroy;
 };
 
+BKTree create_bk_tree(MatchType type){
+    BKTree new_tree = malloc(sizeof(*new_tree));
+
+    if (type == MT_EDIT_DIST)
+        new_tree->compare = edit_distance;
+    else if (type == MT_HAMMING_DIST)
+        new_tree->compare = hamming_distance;
+    else{
+        free(new_tree);
+        return NULL;
+    }
+
+    new_tree->root = NULL;
+    
+    return new_tree;
+
+}   
+
+ErrorCode insert_bknode(BKTree bktree, Pointer value){
+
+}
+
+void bk_destroy(BKTree bktree, DestroyFunc destroy){
+
+}
+
+int hamming_distance(Pointer value1, Pointer value2){
+    String word1 = value1;
+
+    String word2 = value2;
+
+    if(strlen(word1) != strlen(word2))
+        return -1;
+
+    int hamming_dist = 0;
+
+
+    for(int i = 0; i < strlen(word1); i++){
+        if(word1[i] != word2[i])
+            hamming_dist++;
+    }
+
+    return hamming_dist;
+
+}
+
+int edit_distance(Pointer value1, Pointer value2){
+    
+
+}
