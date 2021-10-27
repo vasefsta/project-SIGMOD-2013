705b60910ed5599a185b4e8856359e000ff1d7ed
vasiliki2000 <sdi1800229@di.uoa.gr>
2021-10-27 11:45:28 +0300

    Update ADTLinkedList_test.c

--- tests/ADTLinkedList_test.c
+++ tests/ADTLinkedList_test.c
@@ -1,17 +1,24 @@
 #include <stdlib.h>
 #include <string.h>
+
 #include "acutest.h"
+#include "ADTEntryList.h"
 
 #include "ADTLinkedList.h"
 
-int compare_queries(Query a, Query b) {
-	return strcmp(a->words,b->words);
+int compare_entries(Entry a, Entry b) {
+	return strcmp(a->word,b->word);
 }
 
-
-void destroy(Query query){
+void destroy_query(Query query) {
     free(query->words);
     free(query);
+}
+
+void destroy_entries(Entry entry){
+    list_destroy(entry->payload, (DestroyFunc)destroy_query);
+    free(entry->word);
+    free(entry);
 }
 
 
@@ -28,11 +35,17 @@ void test_insert(){
 
     TEST_ASSERT(list != NULL);
 
-    Query query1 = malloc(sizeof(*query1));
-    Query query2 = malloc(sizeof(*query1));
-    Query query3 = malloc(sizeof(*query1));
-    Query query4 = malloc(sizeof(*query1));
+    Entry entry1 = malloc(sizeof(*entry1));
+    Entry entry2 = malloc(sizeof(*entry2));
    
+
+    entry1->word = strdup("hello");
+    entry1->payload = list_create();
+
+    Query query1 = malloc(sizeof(*query1));
+    Query query2 = malloc(sizeof(*query2));
+    Query query3 = malloc(sizeof(*query3));
+    Query query4 = malloc(sizeof(*query4));
 
     query1->words = strdup("Kostis palamas giolo");
     query1->length = 3;
@@ -46,34 +59,54 @@ void test_insert(){
     query4->words = strdup("werew rewrqc dvs");
     query4->length = 3;
 
+    list_insert(entry1->payload, query1);
+    list_insert(entry1->payload, query2);
+    list_insert(entry1->payload, query3);
+    list_insert(entry1->payload, query4);
 
-    list_insert(list, query1);
-    list_insert(list, query2);
-    list_insert(list, query3);
-    list_insert(list, query4);
+    list_insert(list, entry1);
 
 
-    TEST_ASSERT(list_size(list) == 4);
+    entry2->word = strdup("bye");
+    entry2->payload = list_create();
 
-    ListNode result = list_find(list, (CompareFunc) compare_queries, query1);
+    Query query5 = malloc(sizeof(*query5));
+    Query query6 = malloc(sizeof(*query6));
+    Query query7 = malloc(sizeof(*query7));
+    Query query8 = malloc(sizeof(*query8));
+
+    query5->words = strdup("Kostis palamas giolo");
+    query5->length = 3;
+
+    query6->words = strdup("HAHAHA AHAHHAAH AHAHAHAH");
+    query6->length = 3;
+
+    query7->words = strdup("sadas dasasdad adsdasdas");
+    query7->length = 3;
+
+    query8->words = strdup("werew rewrqc dvs");
+    query8->length = 3;
+
+    list_insert(entry2->payload, query5);
+    list_insert(entry2->payload, query6);
+    list_insert(entry2->payload, query7);
+    list_insert(entry2->payload, query8);
+
+    list_insert(list, entry2);
+
+
+    TEST_ASSERT(list_size(list) == 2);
+
+    ListNode result = list_find(list, (CompareFunc) compare_entries, entry1);
 
     TEST_ASSERT(result != NULL);
 
-    result = list_find(list, (CompareFunc) compare_queries, query2);
-
-    TEST_ASSERT(result != NULL);
-
-    result = list_find(list, (CompareFunc) compare_queries, query3);
-
-    TEST_ASSERT(result != NULL);
-
-    result = list_find(list, (CompareFunc) compare_queries, query4);
+    result = list_find(list, (CompareFunc) compare_entries, entry2);
 
     TEST_ASSERT(result != NULL);
     
     
-    list_destroy(list, (DestroyFunc) destroy);
-
+    list_destroy(list, (DestroyFunc) destroy_entries);
 }
 
 
