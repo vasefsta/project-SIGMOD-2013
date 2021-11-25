7fc8d2c27e4eec82ded49359134673a0a9d2d74d
Nikos <sdi1800221@di.uoa.gr>
2021-11-25 19:40:04 +0200

    test

--- tests/ADTIndex_test.d
+++
@@ -1,4 +0,0 @@
-ADTIndex_test.o: ADTIndex_test.c ../include/acutest.h \
- ../include/ADTIndex.h ../include/common_types.h ../include/core.h \
- ../include/ADTEntryList.h ../include/ADTLinkedList.h ../include/ADTMap.h \
- ../include/misc.h
--- tests/misc_test.d
+++
@@ -1,3 +0,0 @@
-misc_test.o: misc_test.c ../include/acutest.h ../include/misc.h \
- ../include/common_types.h ../include/core.h ../include/ADTEntryList.h \
- ../include/ADTLinkedList.h ../include/ADTMap.h
