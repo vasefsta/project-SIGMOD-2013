296304b547b4339fd01617e72a9d245c6e67119a
Nikos <sdi1800221@di.uoa.gr>
2021-12-15 16:11:55 +0200

    Added comments for Index

--- include/ADTIndex.h
+++ include/ADTIndex.h
@@ -12,17 +12,24 @@
 
 typedef struct index* Index;
 
-Index create_index(MatchType matchtype, CompareFunc compare,  int size);            // Return pointer to an empty index with size and compare function
+// Return pointer to an empty index with size and compare function
+Index create_index(MatchType matchtype, CompareFunc compare,  int size);            
 
-ErrorCode build_entry_index(Index index, const EntryList entrylist);                // Insert entrylist's entries in index
+// Get all entries from entrylist and build a Map or BKTree from them.
+ErrorCode build_entry_index(Index index, const EntryList entrylist);                
 
-ErrorCode lookup_entry_index(Index index, String word, int threshold,               // Insert entries with threshold in range in result
+// If index is a bktree then calls bk_find to complete the task.
+// If index is a Map then it checks if this word makes any queries 
+// fully match the document. If this happens then inserts these queries 
+// to complete_queries.
+ErrorCode lookup_entry_index(Index index, String word, int threshold, 
 Map map_result, List complete_queries, CompareFunc compare_queries);
 
+// Return's the number of elements in index.
 int size_index(Index index);
 
+// Return's index's index.
 Pointer index_index(Index index);
 
-Pointer find_index(Index index, Entry entry);
-
-ErrorCode destroy_entry_index(Index index, DestroyFunc destroy);                                         // Destroy index
+// Destroys index and destroyes index's elements with DestroyFunc.
+ErrorCode destroy_entry_index(Index index, DestroyFunc destroy);
--- modules/ADTIndex.c
+++ modules/ADTIndex.c
@@ -13,26 +13,31 @@
 #include "misc.h"
 
 unsigned int hash_function(Entry entry) {
+    // Hash entry->word and return hash_value.
 	return hash_string(entry->word);
 }
 
 struct index {
-    Pointer index;
-    MatchType matchtype;
-    int size;
-};
+    Pointer index;          // Holds a Pointer to an index.               
+    MatchType matchtype;    // MT_EXACT_MATCH or MT_EDIT_DIST or MT_HAMMING_DIST.
+    int size;               // Number of elements in index.
+};  
 
 
 Index create_index(MatchType matchtype, CompareFunc compare, int size) {
-    Index index = malloc(sizeof(*index));                                   // Create empty index
+    // Create empty index
+    Index index = malloc(sizeof(*index));                                   
 
+    // If matchtype is MT_EXACT_MATCH create map for index
     if (matchtype == MT_EXACT_MATCH){
-        index->index = (Map) map_create((CompareFunc) compare, size);       // Create map for index
+        index->index = (Map) map_create((CompareFunc) compare, size);       
         map_set_hash_function(index->index, (HashFunc) hash_function);
     }
+    // If matchtype is MT_HAMMING_DIST or MT_EDIT_DIST create a bktree for index.
     else if (matchtype == MT_HAMMING_DIST || matchtype == MT_EDIT_DIST)
-        index->index = bk_create(matchtype);                                // Create bk tree for index
+        index->index = bk_create(matchtype);                                
     
+    // Assign values.
     index->matchtype = matchtype;
     index->size = 0;
 
@@ -41,70 +46,97 @@ Index create_index(MatchType matchtype, CompareFunc compare, int size) {
 
 ErrorCode build_entry_index(Index index, const EntryList entrylist) {
     ErrorCode errcode;
-    for (Entry entry = get_first(entrylist);                            // Traverse through entrylist
-        entry != NULL; 
-        entry = get_next(entrylist, entry)) {
 
+    // For every entry in entrylist.
+    for (Entry entry = get_first(entrylist); entry != NULL; entry = get_next(entrylist, entry)) {
+
+            // If matchtype = MT_EXACT_MATCH insert in map.
             if (index->matchtype == MT_EXACT_MATCH)
-                errcode = map_insert((Map)index->index, entry);         
+                errcode = map_insert((Map)index->index, entry);       
+
+            // If matchtype = MT_EDIT_DIST or MT_HAMMING_DIST insert in bktree.
             else if (index->matchtype == MT_HAMMING_DIST || index->matchtype == MT_EDIT_DIST)
                 errcode = bk_insert((BKTree) index->index, entry);
 
+            // If an error occured exit.
             if (errcode == EC_FAIL)
                 return EC_FAIL;
             
+            // Increase index's size.
             index->size++;
     }
-
-    
-
     return EC_SUCCESS;
 }
 
 
 ErrorCode lookup_entry_index(Index index, String word, int threshold, Map map_result, List complete_queries, CompareFunc compare_query) {
 
+    // If matchtype is MT_EDIT_DIST or MT_HAMMING_DIST call bk_find.
     if (index->matchtype == MT_EDIT_DIST || index->matchtype == MT_HAMMING_DIST)
         bk_find((BKTree)index->index, map_result, complete_queries, compare_query, word, threshold);
+    
+    // if matchtype is MT_EXACT_MATCH. 
     else if (index->matchtype == MT_EXACT_MATCH) {
+        // Create a dummy entry with entry.word = word.
         struct entry entry;
-
         entry.word = word;
         entry.payload = NULL;
 
-        Entry res = map_find((Map)index->index, &entry);                 // Check if it exists in index      // Exact match
+        // Check if entry exists in index.
+        Entry res = map_find((Map)index->index, &entry);                 
         
-        if(res != NULL) {                                                // If not
+        // If entry exists
+        if(res != NULL) {
+            // For every Query in entry's payload.
             for (ListNode node = list_first(res->payload); node != NULL; node = list_find_next(node)) {
                 Query query = list_node_value(node);
                 
+                // Create a dummy special.
                 struct special tmpspecial;
-
                 tmpspecial.query = query;
                 tmpspecial.words = NULL;
+
+                // Check if Special exists in map_result.
                 Special special = map_find(map_result, &tmpspecial);
 
+                // If no special was found.
                 if (!special) {
+                    // Create a new Special.
                     special = malloc(sizeof(*special));
-
                     special->query = query;
                     special->words = list_create((CompareFunc) strcmp);
                     
+                    // Insert it to map_result.
                     map_insert(map_result, special);
 
+                    // Insert entry's word to special's words.
                     list_insert(special->words, entry.word);
 
+                    // If size of special words is equal to query's length 
+                    // means that the query fully matches this document.
                     if (list_size(special->words) == special->query->length) {
-                       QueryID* queryid = malloc(sizeof(*queryid));
-                        *queryid = query->queryID;
-                        list_insert(complete_queries, queryid);
-                    }
-                } else if (!list_find(special->words, entry.word)){
-                    list_insert(special->words, entry.word);
-
-                    if (list_size(special->words) == special->query->length) {
+                        // Create a QueryID.
                         QueryID* queryid = malloc(sizeof(*queryid));
                         *queryid = query->queryID;
+                        // Insert queryID to complete_queries list.
+                        list_insert(complete_queries, queryid);
+                    }
+
+                }
+                // If entry.word does not exist in special->words.
+                // We check this so we do not store duplicate words
+                // in special->words.
+                else if (!list_find(special->words, entry.word)){
+                    // Insert word to special->words/
+                    list_insert(special->words, entry.word);
+
+                    // If size of special words is equal to query's length 
+                    // means that the query fully matches this document.
+                    if (list_size(special->words) == special->query->length) {
+                        // Create a QueryID.
+                        QueryID* queryid = malloc(sizeof(*queryid));
+                        *queryid = query->queryID;
+                        // Insert queryID to complete_queries list.
                         list_insert(complete_queries, queryid);
                     }
                 }
@@ -118,19 +150,27 @@ ErrorCode lookup_entry_index(Index index, String word, int threshold, Map map_re
 
 
 int size_index(Index index) {
+    // Return index's size.
     return index->size;
 }
 
 Pointer index_index(Index index){
+    // Return index's index (Bktree or Map).
     return index->index;
 }
 
 ErrorCode destroy_entry_index(Index index, DestroyFunc destroy) {
+    // If matchtype is MT_EDIT_DIST or MT_HAMMING_DIST
     if (index->matchtype == MT_EDIT_DIST || index->matchtype == MT_HAMMING_DIST) 
+        // Destroy bktree.
         bk_destroy((BKTree)index->index, destroy);
+
+    // If matchtype is MT_EXACT_MATCH
     else 
+        // Destroy Map.
         map_destroy((Map)index->index, destroy);
 
+    // Free index.
     free(index);
 
     return EC_SUCCESS;
